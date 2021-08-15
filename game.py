fd9ff1d7251fdee445b1af5689f9993dd0a12bf0
ARCHITECT <greatraksin@icloud.com>
2021-08-15 14:08:02 +0300

    Adding game template. Making classes for game objects🌠

---
+++ game.py
@@ -0,0 +1,11 @@
+from objects import GameRound, Snake, Food
+
+
+def main():
+    game = GameRound()
+    player = Snake()
+    apple = Food()
+
+
+if __name__ == '__main__':
+    main()
---
+++ objects.py
@@ -0,0 +1,14 @@
+import pygame
+import random
+import time
+import sys
+
+class GameRound:
+    """Round handler class"""
+    pass
+
+class Snake:
+    pass
+
+class Food:
+    pass
